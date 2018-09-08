ca07ff94a837561214987f3ba8fe805235ce709f
Neel3 <43052434+Neel3@users.noreply.github.com>
2018-09-07 21:32:32 -0400

    Update FF_Data_Automater.java

--- FF_Data_Automater.java
+++ FF_Data_Automater.java
@@ -1,63 +1,74 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class FF_Data_Automater {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            // importing txt file
-            File txtFile = new File("D:\\Fantasy Football\\Analytics\\2018\\Cousins\\Season\\Data" +
-                    "\\Week.txt");
+        // exporting to txt file
+
+
+        // importing data from txt file
+        File txtFile = new File("D:\\Fantasy Football\\Analytics\\2018\\Cousins\\Season\\Data" +
+                "\\Week.txt");
+        try {
+            Scanner txtScanner = new Scanner(txtFile);
             try {
-                Scanner txtScanner = new Scanner(txtFile);
+                PrintStream ps2 = new PrintStream("D:\\Fantasy Football\\Analytics\\2018\\Cousins" +
+                        "\\Season" +
+                        "\\Data\\Week_output.txt");
 
-                // getting input from txt file
-                int week = txtScanner.nextInt();
-                System.out.println("Week: " + week);
-                    // scores and records for each team
-                double score, skip;
-                int wins, losses, ties;
-                String team, record;
-                try {
-                    for (int i = 1; i < 12; i++) {
+            // getting input from txt file
+            int week = txtScanner.nextInt();
+            System.out.println("Week: " + week);
+            // scores and records for each team
+            double score, skip;
+            int wins, losses, ties;
+            String team, record;
+            try {
+                for (int i = 1; i < 12; i++) {
 //                    System.out.println("i: " + i);
 //                    System.out.println("i % 2: " + (i % 2));
 
-                        // odd teams
-                        if (i % 2 != 0) {
+                    // odd teams
+                    if (i % 2 != 0) {
 //                        System.out.println("Method odd");
-                            score = txtScanner.nextDouble();
-                            System.out.println("Score: " + score);
-                            // skip projected score
-                            skip = txtScanner.nextDouble();
-                            team = txtScanner.nextLine();
-                            team = txtScanner.nextLine();
-                            System.out.println("Team: " + team);
-                            record = txtScanner.nextLine();
-                            System.out.println("Record: " + record);
-                            continue;
-                        }
-
-                        // even teams
-                        if (i % 2 == 0) {
-//                        System.out.println("Method even");
-                            // skip logo
-                            txtScanner.nextLine();
-                            score = txtScanner.nextDouble();
-                            System.out.println("Score: " + score);
-                            // skip projected score
-                            skip = txtScanner.nextDouble();
-                            // skip logo
-                            txtScanner.nextLine();
-                            txtScanner.nextLine();
-                            team = txtScanner.nextLine();
-                            System.out.println("Team: " + team);
-                            record = txtScanner.nextLine();
-                            System.out.println("Record: " + record);
-                            continue;
-                        }
+                        score = txtScanner.nextDouble();
+                        System.out.println("Score: " + score);
+                        // skip projected score
+                        skip = txtScanner.nextDouble();
+                        team = txtScanner.nextLine();
+                        team = txtScanner.nextLine();
+                        System.out.println("Team: " + team);
+                        record = txtScanner.nextLine();
+                        System.out.println("Record: " + record);
+                        continue;
                     }
+
+                    // even teams
+                    if (i % 2 == 0) {
+//                        System.out.println("Method even");
+                        // skip logo
+                        txtScanner.nextLine();
+                        score = txtScanner.nextDouble();
+                        System.out.println("Score: " + score);
+                        // skip projected score
+                        skip = txtScanner.nextDouble();
+                        // skip logo
+                        txtScanner.nextLine();
+                        txtScanner.nextLine();
+                        team = txtScanner.nextLine();
+                        System.out.println("Team: " + team);
+                        record = txtScanner.nextLine();
+                        System.out.println("Record: " + record);
+                        continue;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 //
 //                    // even teams
 //                    if ((i % 2 == 0)) {
@@ -107,16 +118,16 @@ public class FF_Data_Automater {
 ////                    txtScanner.skip("-");
 ////                    ties = txtScanner.nextInt();
 ////                    System.out.println("Ties: " + ties);
-                } catch (Exception e1) {
+            } catch (Exception e1) {
 //                    e1.printStackTrace();
-                }
-
-
-
-            } catch (FileNotFoundException e2) {
-                System.out.println("File not found.");
-                e2.printStackTrace();
             }
 
+
+
+        } catch (FileNotFoundException e2) {
+            System.out.println("File not found.");
+            e2.printStackTrace();
         }
+
+    }
 }
