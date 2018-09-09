b16a7cc14a53bb8c0e530ac3f7c9a8ca30578eeb
Neel3 <43052434+Neel3@users.noreply.github.com>
2018-09-08 20:06:55 -0400

    Update FF_Data_Automater.java

--- FF_Data_Automater.java
+++ FF_Data_Automater.java
@@ -1,6 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -88,27 +88,71 @@ public class FF_Data_Automater {
      */
     public void teamToRealName() {
 
-        
         // input HTML file
         File names = new File("D:\\Fantasy Football\\Analytics\\2018\\Cousins\\Season\\Data" +
-                "\\Names.html");
+                "\\Scores.html");
         Document doc1 = null;
         try {
-            doc1 = (Document) Jsoup.parse(names, "UTF-8", " ");
+             doc1 = (Document) Jsoup.parse(names, "UTF-8", " ");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // search for team and real names
-        Element nameSearcher = doc1.getElementById("teams");
-        System.out.println("nameSearcher: " + nameSearcher);
-
-        Element link = doc1.getElementById("team-3-name");
-        System.out.println("link: " + link);
-        for (int i = 1; i < 11; i++) {
-            Element link2 = doc1.getElementById("team-" + i + "-name");
-            System.out.println("link[" + i + "]: " + link2);
+        
+        // Store team ID into arrays
+        Elements x4 = doc1.getElementsByClass("F-link");
+//        System.out.println("x4: " + x4);
+        String x5 = x4.toString();
+        System.out.println("x5: " + x5);
+        String x6 = x5.substring(35,36);
+        System.out.println("x6: " + x6);
+        Scanner x7 = new Scanner(x5);
+        int teamID[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            String x8 = x7.nextLine();
+//            Scanner x9 = new Scanner(x8.substring(35,36));
+            String x10;
+            if (x8.contains("10")) {
+                x10 = x8.substring(35,37);
+            }
+            else {
+                x10 = x8.substring(35, 36);
+            }
+            int x11 = Integer.parseInt(x10);
+            teamID[i] = x11;
+            System.out.println("teamID[" + i + "]: " + teamID[i]);
+//            if (x8.substring(36,37).)
         }
+
+        //Scores
+        Elements x1 = doc1.select("div.Fz-lg");
+        String x2 = x1.select("div.Fz-lg").text();
+//        System.out.println("x1: " + x1);
+        System.out.println("x2: " + x2);
+        Scanner x3 = new Scanner(x2);
+
+        // skip words
+        for (int i = 0; i < 4; i++) {
+            x3.next();
+        }
+        // print scores
+        for (int i = 0; i < 10; i++) {
+//            System.out.println("[" + i + "]: " + x3.nextDouble());
+            System.out.println("teamID[" + teamID[i] + "]: " + x3.nextDouble());
+        }
+
+
+
+
+
+//        Element nameSearcher = doc1.getElementById("teams");
+//        System.out.println("nameSearcher: " + nameSearcher);
+//
+//        Element link = doc1.getElementById("team-3-name");
+//        System.out.println("link: " + link);
+//        for (int i = 1; i < 11; i++) {
+//            Element link2 = doc1.getElementById("team-" + i + "-name");
+//            System.out.println("link[" + i + "]: " + link2);
+//        }
 
     }
 
